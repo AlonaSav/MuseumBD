ede96bcddca5bb9cfd5affa51fb97e4735c23c9d
Alona Savchenko <alyona1795@gmail.com>
2017-06-13 10:36:02 +0300

    create database

--- MuseumBD/Program.cs
+++ MuseumBD/Program.cs
@@ -19,6 +19,19 @@ namespace MuseumBD
             Application.Run(new Form1());
             string databaseName = @"MuseumBD.db";
             SQLiteConnection.CreateFile(databaseName);
+            SQLiteConnection connection = new SQLiteConnection(string.Format("Data Sourse = {0}", databaseName));
+            SQLiteCommand command = new SQLiteCommand("CREATE TABLE IF NOT EXISTS Article (Id_article INTEGER PRIMARY KEY AUTO_INCREMENT, Title TEXT, Date_pub DATE, Text TEXT);", connection);
+            connection.Open();
+            command.ExecuteNonQuery();
+            connection.Close();
+            command = new SQLiteCommand("CREATE TABLE IF NOT EXISTS Teacher (Id_teacher INTEGER PRIMARY KEY AUTO_INCREMENT, Name TEXT, Birthday DATE, Department TEXT);", connection);
+            connection.Open();
+            command.ExecuteNonQuery();
+            connection.Close();
+            command = new SQLiteCommand("CREATE TABLE IF NOT EXISTS Article (Id_article INTEGER PRIMARY KEY AUTO_INCREMENT, Title TEXT, Date_pub DATE, Text TEXT);", connection);
+            connection.Open();
+            command.ExecuteNonQuery();
+            connection.Close();
         }
     }
 }
